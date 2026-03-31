f2710c958173ab95624ec4d6eef233c91217abe8
Sebastian Lopez <sebas3004tian@gmail.com>
2026-03-31 17:21:06 -0500

    chore: init commit

---
+++ src/main/java/com/foodcourt/traceability_microservice_foodcourt/TraceabilityMicroserviceFoodcourtApplication.java
@@ -0,0 +1,13 @@
+package com.foodcourt.traceability_microservice_foodcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceabilityMicroserviceFoodcourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceabilityMicroserviceFoodcourtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodcourt/traceability_microservice_foodcourt/TraceabilityMicroserviceFoodcourtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodcourt.traceability_microservice_foodcourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceabilityMicroserviceFoodcourtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
